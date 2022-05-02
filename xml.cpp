676e29c2f4d960134dee1fd9b87d16d423126c3c
akusgulu <akusgulu@gmail.com>
2022-05-02 09:54:08 +0300

    Basic XML error handling

--- xml.cpp
+++ xml.cpp
@@ -33,14 +33,26 @@ vector<vec3> str_to_vv3(const char *str)
     return vv3;
 }
 
-void field_error(const char *field_name)
+bool is_valid(const char_t *p, const string &error_msg,bool& err)
 {
-    cerr << "Error loading field: " << field_name << endl;
+    if (p == "")
+    {
+        cerr << "XML error: " << error_msg << " not found" << endl;
+        err |= true;
+        return false;
+    }
+    err |= false;
+    return true;
+}
+
+inline bool is_valid(const char_t *p, const string &id, const string &error_msg,bool& err)
+{
+    return is_valid(p, id + error_msg,err);
 }
 
 bool scene_from_file(scene_st &scene, const char *path)
 {
-
+    bool err = false;
     xml_document doc;
     xml_parse_result res = doc.load_file(path, parse_trim_pcdata);
 
@@ -59,61 +71,97 @@ bool scene_from_file(scene_st &scene, const char *path)
     xml_node objs = sc.child("objects");
 
     vector<double> tok;
-    string buf;
 
-    scene.max_depth = stoi(sc.child_value("maxraytracedepth"));
-    scene.background = v_to_v3(tokenize(sc.child_value("background")));
+    if (is_valid(sc.child_value("maxraytracedepth"), "-maxraytracedepth-",err))
+        scene.max_depth = stoi(sc.child_value("maxraytracedepth"));
 
-    scene.camera.position = v_to_v3(tokenize(camera.child_value("position")));
-    scene.camera.w = -v_to_v3(tokenize(camera.child_value("gaze")));
-    scene.camera.v = v_to_v3(tokenize(camera.child_value("up")));
+    if (is_valid(sc.child_value("background"), "-background-",err))
+        scene.background = v_to_v3(tokenize(sc.child_value("background")));
+
+    if (is_valid(camera.child_value("position"), "-camera.position-",err))
+        scene.camera.position = v_to_v3(tokenize(camera.child_value("position")));
+
+    if (is_valid(camera.child_value("gaze"), "-camera.gaze-",err))
+        scene.camera.w = -v_to_v3(tokenize(camera.child_value("gaze")));
+
+    if (is_valid(camera.child_value("position"), "-camera.position-",err))
+        scene.camera.v = v_to_v3(tokenize(camera.child_value("up")));
+
     scene.camera.u = cross(scene.camera.v, scene.camera.w);
 
-    tok = tokenize(camera.child_value("nearplane"));
+    if (is_valid(camera.child_value("nearplane"), "-camera.nearplane-",err))
+        tok = tokenize(camera.child_value("nearplane"));
     scene.camera.np[LEFT] = tok[0];
     scene.camera.np[RIGHT] = tok[1];
     scene.camera.np[BOTTOM] = tok[2];
     scene.camera.np[TOP] = tok[3];
 
-    scene.camera.near_distance = stod(camera.child_value("neardistance"));
-    tok = tokenize(camera.child_value("imageresolution"));
+    if (is_valid(camera.child_value("neardistance"), "-camera.neardistance-",err))
+        scene.camera.near_distance = stod(camera.child_value("neardistance"));
+
+    if (is_valid(camera.child_value("imageresolution"), "camera.imageresolution-",err))
+        tok = tokenize(camera.child_value("imageresolution"));
     scene.camera.nx = tok[0];
     scene.camera.ny = tok[1];
 
-    scene.ambient_light = v_to_v3(tokenize(lights.child_value("ambientlight")));
+    if (is_valid(lights.child_value("ambientlight"), "-ambientlight-",err))
+        scene.ambient_light = v_to_v3(tokenize(lights.child_value("ambientlight")));
 
     pointlight_st p;
     for (auto light : lights.children("pointlight"))
     {
-        // cout << light.attribute("id").value() << endl;
-        p.position = v_to_v3(tokenize(light.child_value("position")));
-        p.intensity = v_to_v3(tokenize(light.child_value("intensity")));
+        string id = light.attribute("id").value();
+        if (is_valid(light.child_value("position"), id, ".position",err))
+            p.position = v_to_v3(tokenize(light.child_value("position")));
+
+        if (is_valid(light.child_value("intensity"), id, ".intensity",err))
+            p.intensity = v_to_v3(tokenize(light.child_value("intensity")));
+
         scene.p_lights.push_back(p);
     }
 
     material_st m;
     for (auto mat : materials.children("material"))
     {
-        m.ambient = v_to_v3(tokenize(mat.child_value("ambient")));
-        m.diffuse = v_to_v3(tokenize(mat.child_value("diffuse")));
-        m.specular = v_to_v3(tokenize(mat.child_value("specular")));
-        m.mirror_refl = v_to_v3(tokenize(mat.child_value("mirrorreflectance")));
-        m.phong_exp = stoi(mat.child_value("phongexponent"));
-        scene.materials[mat.attribute("id").value()] = m;
+        string id = mat.attribute("id").value();
+        cerr << id << endl;
+        if (is_valid(mat.child_value("ambient"), id, ".ambient",err))
+            m.ambient = v_to_v3(tokenize(mat.child_value("ambient")));
+
+        if (is_valid(mat.child_value("diffuse"), id, ".diffuse",err))
+            m.diffuse = v_to_v3(tokenize(mat.child_value("diffuse")));
+
+        if (is_valid(mat.child_value("specular"), id, ".specular",err))
+            m.specular = v_to_v3(tokenize(mat.child_value("specular")));
+
+        if (is_valid(mat.child_value("mirrorreflectance"), id, ".mirrorreflectance",err))
+            m.mirror_refl = v_to_v3(tokenize(mat.child_value("mirrorreflectance")));
+
+        if (is_valid(mat.child_value("phongexponent"), id, ".phongexponent",err))
+            m.phong_exp = stoi(mat.child_value("phongexponent"));
+
+        scene.materials[id] = m;
     }
-    auto verts = str_to_vv3(sc.child_value("vertexdata"));
+    vector<vec3> verts;
+    if (is_valid(sc.child_value("vertexdata"),".vertexdata",err))
+        verts = str_to_vv3(sc.child_value("vertexdata"));
 
     mesh_st mesh;
     for (auto o : objs.children("mesh"))
     {
-        mesh.material_id = o.child_value("materialid");
-        auto faces = str_to_vv3(o.child_value("faces"));
-        for (auto f : faces)
+        string id = o.attribute("id").value();
+        if (is_valid(o.child_value("materialid"), id, ".materialid",err))
+            mesh.material_id = o.child_value("materialid");
+        if (is_valid(o.child_value("faces"), id, ".faces",err))
         {
-            mesh.faces.push_back({verts[f.x - 1], verts[f.y - 1], verts[f.z - 1]});
+            auto faces = str_to_vv3(o.child_value("faces"));
+            for (auto f : faces)
+            {
+                mesh.faces.push_back({verts[f.x - 1], verts[f.y - 1], verts[f.z - 1]});
+            }
         }
         scene.objects.push_back(mesh);
     }
 
-    return true;
+    return err;
 }
